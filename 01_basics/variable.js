d7f9c57862df35cf2f79e9eb6e61955667fc021b
nikita-soni-8993 <61220665+nikita-soni-8993@users.noreply.github.com>
2025-03-27 22:15:38 +0000

    basic-var

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const pi = 3.14; // value cannot be changed
+let num= 123;  // let is scoped to a block 
+var names = "nikita"   // var is not used in new JS. It had scope issues.
+
+cityName = "jaipur";  // in JS even if you dont declare variable let or const it will default take it as let and its hoisted at top.
+
+console.table([pi,num, names, cityName]);
